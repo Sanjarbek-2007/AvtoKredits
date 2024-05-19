6fe8d93580074e33ed70fa58d446390097159ca1
Patrick <doniyorkhudayarov@gmail.com>
2024-05-19 11:59:28 +0500

    do Post in the backend

--- src/main/java/uz/yusa/avtokredits/config/WebConfiguration.java
+++ src/main/java/uz/yusa/avtokredits/config/WebConfiguration.java
@@ -17,7 +17,7 @@ public class WebConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Adjust this as per your frontend origin
+                        .allowedOrigins("http://localhost:3000", "http://localhost:3001") // Adjust this as per your frontend origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
